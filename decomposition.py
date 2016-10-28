1f020afbfc0ba2695726fff49e85c57247799d22
leotrs <leo@leotrs.com>
2016-10-27 22:45:03 -0400

    algorithm now merges P-nodes.

--- decomposition.py
+++ decomposition.py
@@ -9,9 +9,72 @@ parallel digraphs. SIAM Journal on Computing, 11(2):298–313, 1982.
 
 """
 
-import networkx as nx
 from itertools import groupby
-from collections import Counter
+import networkx as nx
+
+
+class DAG(nx.MultiDiGraph):
+    """Two Terminal Series Parallel Directed Acyclic Graph."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @staticmethod
+    def read_dag():
+        """Read a DAG from an edgelist file.  All nodes labels must be unique."""
+        dag = DAG()
+
+        num_edges = int(input())
+        for _ in range(num_edges):
+            source, target = input().split(' ')
+            dag.add_edge(source, target)
+
+        return dag
+
+    def get_source(self):
+        """Return the source of the DAG."""
+        # FIXME: for now, we're forcing the source to have the lowest label
+        return min(self.nodes())
+
+    def get_sink(self):
+        """Return the sink of the DAG."""
+        # FIXME: for now, we're forcing the sink to have the highest label
+        return max(self.nodes())
+
+    def get_sole_parent(self, node):
+        """Return the parent of node, given that it only has one predecessor."""
+        in_edges = self.in_edges(node)
+        assert len(in_edges) == 1
+        return in_edges[0][0]
+
+    def get_sole_child(self, node):
+        """Return the child of node, given that it only has one successor."""
+        out_edges = self.out_edges(node)
+        assert len(out_edges) == 1
+        return out_edges[0][1]
+
+    def edge_repetitions(self, node):
+        """Each edge incident to node along with a list of its replicas.
+
+        Return: a list of the form [(edge1, [e11, e12, ...]), ...], where
+        edgei is incident to node, and eij is a triplet of (node1, node2,
+        key), where either node1 or node2 is equal to node, and key is the
+        unique edge identifier.
+
+        """
+        # When called with keys=True, these methods return a list of tuples
+        # of the form (src, tgt, key), representing an edge from src to
+        # tgt, with unique identifier key.  Since in NetworkX Graph classes
+        # the edges are stored in a dictionary, we need the key to access
+        # and delete them correctly.  If we just did dag.remove_edge(src,
+        # tgt), we could be removing an edge that we still have yet to
+        # analyze, we must call dag.remove_edge(src, tgt, key) instead.
+        # This is only relevant if there are multiedges between src and
+        # tgt.
+        all_edges = self.out_edges(node, keys=True) + self.in_edges(node, keys=True)
+        grouped_edges = groupby(all_edges, key=lambda e: (e[0], e[1]))
+        return [(edge, list(repetitions)) for edge, repetitions
+                in grouped_edges]
 
 
 class DecompositionTree(nx.Graph):
@@ -19,6 +82,7 @@ class DecompositionTree(nx.Graph):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.root = None
 
     def _new_label(self, name):
         """Return a string based on label for valid use as a node label.
@@ -49,6 +113,7 @@ class DecompositionTree(nx.Graph):
         """Merge node1, node2 with a P-node."""
         label = self._new_label('P')
         self.add_node(label)
+        self.root = label
         self.add_edge(label, node1)
         self.add_edge(label, node2)
 
@@ -58,6 +123,7 @@ class DecompositionTree(nx.Graph):
         """Merge node1, node2 with an S-node."""
         label = self._new_label('S')
         self.add_node(label)
+        self.root = label
         self.add_edge(label, node1)
         self.add_edge(label, node2)
 
@@ -94,189 +160,167 @@ class DecompositionTree(nx.Graph):
 
         return self._new_snode(node1, node2)
 
+    def parallel_reduce(self, dag, node):
+        """Perform all possible parallel reductions on edges incident to node.  Add
+        the reduced nodes to the decomposition tree.
 
-def read_dag():
-    """Read a DAG from an edgelist file.  All nodes labels must be unique."""
-    dag = nx.MultiDiGraph()
+        """
+        # For every neighbor of node, remove all repeated multi-edges of
+        # the form node -> neighbor or neighbor -> node, and just leave a
+        # single one.
+        edge_repetitions = dag.edge_repetitions(node)
 
-    num_edges = int(input())
-    for _ in range(num_edges):
-        source, target = input().split(' ')
-        dag.add_edge(source, target)
+        # At each iteration, we merge two replicated edges into one.  This
+        # new edge will carry a 'decomposition_node' label, pointing to the
+        # node in the decomposition tree that stands for the merge.  If the
+        # edges we are merging already have 'decomposition_node' labels, we
+        # must merge the node pointed to by the labels.  Otherwise, we must
+        # create a new node.  After each merge, we recompute all edges.
+        while any(len(reps) > 1 for edge, reps in edge_repetitions):
+            multi_edges = ((e, reps) for e, reps in edge_repetitions
+                           if len(reps) > 1)
 
-    return dag
+            try:
+                multi_edge, repetitions = next(multi_edges)
+                source, target = multi_edge
+                key1 = repetitions[0][-1]
+                key2 = repetitions[1][-1]
+                label1 = dag.edge[source][target][key1].get('decomposition_node')
+                label2 = dag.edge[source][target][key2].get('decomposition_node')
 
+                dag.remove_edge(source, target, key1)
+                dag.remove_edge(source, target, key2)
 
-def get_source(dag):
-    """Return the source of the DAG."""
-    # FIXME: for now, we're forcing the source to have the lowest label
-    return min(dag.nodes())
+                # print('P-reducing {}-{}, ({}, {})'.format(source, target,
+                #                                           label1, label2))
 
+                pnode = self.add_pnode(source, target, label1, label2)
+                dag.add_edge(source, target, decomposition_node=pnode)
 
-def get_sink(dag):
-    """Return the sink of the DAG."""
-    # FIXME: for now, we're forcing the sink to have the highest label
-    return max(dag.nodes())
+            except StopIteration:
+                break
 
+            edge_repetitions = dag.edge_repetitions(node)
 
-def get_sole_parent(dag, node):
-    """Return the parent of node, given that it only has one predecessor."""
-    in_edges = dag.in_edges(node)
-    assert len(in_edges) == 1
-    return in_edges[0][0]
+    def series_reduce(self, dag, node, parent=None, child=None):
+        """Reduce a node that lies between just one parent and just one child.  Add
+        the reduced nodes to the decomposiiton tree.
 
+        """
+        if parent is None:
+            parent = dag.get_sole_parent(node)
+        if child  is None:
+            child = dag.get_sole_child(node)
 
-def get_sole_child(dag, node):
-    """Return the child of node, given that it only has one successor."""
-    out_edges = dag.out_edges(node)
-    assert len(out_edges) == 1
-    return out_edges[0][1]
+        # print('S-reducing {}-{}-{}'.format(parent, node, child))
 
+        label1 = dag.edge[parent][node][0].get('decomposition_node')
+        label2 = dag.edge[node][child][0].get('decomposition_node')
 
-def parallel_reduce(dag, node, tree):
-    """Perform all possible parallel reductions on edges incident to node.  Add
-    the reduced nodes to the decomposition tree.
+        dag.remove_edge(parent, node)
+        dag.remove_edge(node, child)
 
-    """
-    # For every neighbor of node, remove all repeated multi-edges of the
-    # form node -> neighbor or neighbor -> node, and just leave a single
-    # one.
+        snode = self.add_snode(parent, node, child, label1, label2)
+        dag.add_edge(parent, child, decomposition_node=snode)
 
-    # When called with keys=True, these methods return a list of tuples of
-    # the form (src, tgt, key), representing an edge from src to tgt, with
-    # unique identifier key.  Since the edges are stored in a dictionary,
-    # we need the key to access/delete them correctly.  If we just did
-    # dag.remove_edge(src, tgt), we could be removing an edge that we still
-    # have yet to analyze.  (This is only relevant if there are multiedges
-    # between src and tgt.)
-    all_edges = dag.out_edges(node, keys=True) + dag.in_edges(node, keys=True)
-    grouped_edges = groupby(all_edges, key=lambda e: (e[0], e[1]))
-    edge_repetitions = [(edge, list(repetitions)) for edge, repetitions
-                        in grouped_edges]
-    # all_edges = Counter(dag.out_edges(node) + dag.in_edges(node))
+    def decompose(self, dag):
+        """Return the decomposition tree of the DAG."""
+        # The TTSP recognition algorithm, from the referenced source.
 
-    # At each iteration, we merge two replicated edges into one.  This new
-    # edge will carry a 'decomposition_node' label, pointing to the node in
-    # the decomposition tree that stands for the merge.  If the edges we
-    # are merging already have 'decomposition_node' labels, we must merge
-    # the node pointed to by the labels.  Otherwise, we must create a new
-    # node.  After each merge, we recompute all edges.
-    while any(len(reps) > 1 for edge, reps in edge_repetitions):
-        multi_edges = ((e, reps) for e, reps in edge_repetitions
-                       if len(reps) > 1)
+        # We maintain a list of vertices that initially includes all vertices
+        # except the source and the sink.
+        source = dag.get_source()
+        sink = dag.get_sink()
+        # print('Source: {}, sink: {}'.format(source, sink))
+        to_visit = set(dag.nodes()).difference(source, sink)
+        # print('to visit: {}'.format(to_visit))
 
-        try:
-            multi_edge, repetitions = next(multi_edges)
-            source, target = multi_edge
-            key1 = repetitions[0][-1]
-            key2 = repetitions[1][-1]
-            label1 = dag.edge[source][target][key1].get('decomposition_node')
-            label2 = dag.edge[source][target][key2].get('decomposition_node')
+        # The algorithm proceeds by removing any vertex v from this list and
+        # performing as many parallel reductions on the edges incident to
+        # (from) it as it is possible.
+        while to_visit:
+            node = to_visit.pop()
+            self.parallel_reduce(dag, node) # modifies dag!
 
-            dag.remove_edge(source, target, key1)
-            dag.remove_edge(source, target, key2)
+            # Now we have either 1. the vertex has a single entering edge and a
+            # single exiting edge, or 2. the vertex still has at least two
+            # distinct in-neighbors or two distinct out-neighbors.  In the
+            # first alternative, the vertex is removed by a series reduction
+            # and the two vertices adjacent to it added to the unsatisfied list
+            # if they are not there already.
+            if dag.in_degree()[node] == 1 and dag.out_degree()[node] == 1:
+                parent = dag.get_sole_parent(node)
+                child = dag.get_sole_child(node)
+                if parent not in [source, sink]:
+                    to_visit.add(parent)
+                if child not in [source, sink]:
+                    to_visit.add(child)
 
-            # print('P-reducing {}-{}, ({}, {})'.format(source, target,
-            #                                           label1, label2))
+                self.series_reduce(dag, node, parent, child) # modifies dag!
 
-            pnode = tree.add_pnode(source, target, label1, label2)
-            dag.add_edge(source, target, decomposition_node=pnode)
+            else:
+                # We've found an invalid node.  This is not a TTSP.
+                print('{} is invalid.'.format(node))
+                return
 
-        except StopIteration:
-            break
+        # This process is repeated until the unsatisfied list becomes empty, at
+        # which point the same process is applied to the source and the sink
+        # (in order to eliminate any multiple edges between them) before
+        # stopping.
+        self.parallel_reduce(dag, source) # modifies dag!
 
-        all_edges = dag.out_edges(node, keys=True) + dag.in_edges(node, keys=True)
-        grouped_edges = groupby(all_edges, key=lambda e: (e[0], e[1]))
-        edge_repetitions = [(edge, list(repetitions)) for edge, repetitions
-                            in grouped_edges]
+        # The following line is not necessary as at this point there are no
+        # more nodes other than source and sink, and all edges must be parallel
+        # between them.
+        # parallel_reduce(dag, sink)
 
+        # The unsatisfied list becomes empty, either because all vertices
+        # (except source and sink) have been deleted by series reductions or
+        # because every remaining vertex has two distinct in-neighbors or two
+        # distinct out-neighbors. In the first case the DAG is TTSP; in the
+        # second it is not.
+        print('Remaining edges in  dag: {}'.format(dag.edges()))
 
-def series_reduce(dag, node, tree, parent=None, child=None):
-    """Reduce a node that lies between just one parent and just one child.  Add
-    the reduced nodes to the decomposiiton tree.
+    def is_pnode(self, node):
+        """Return whether or not node is a P-node."""
+        return node.startswith('P')
 
-    """
-    if parent is None:
-        parent = get_sole_parent(dag, node)
-    if child  is None:
-        child = get_sole_child(dag, node)
+    def merge_pnodes(self):
+        """Collapse each P-node into one."""
+        self._merge_pnodes_from(self.root)
 
-    # print('S-reducing {}-{}-{}'.format(parent, node, child))
+    def _merge_pnodes_from(self, node, parent=None):
+        """Traverse in post-order and merge all P-nodes starting from node."""
+        children = self.neighbors(node)
 
-    label1 = dag.edge[parent][node][0].get('decomposition_node')
-    label2 = dag.edge[node][child][0].get('decomposition_node')
-
-    dag.remove_edge(parent, node)
-    dag.remove_edge(node, child)
-
-    snode = tree.add_snode(parent, node, child, label1, label2)
-    dag.add_edge(parent, child, decomposition_node=snode)
-
-
-def decompose(dag):
-    """Return the decomposition tree of the DAG."""
-    # The TTSP recognition algorithm, from the referenced source.
-    tree = DecompositionTree()
-
-    # We maintain a list of vertices that initially includes all vertices
-    # except the source and the sink.
-    source = get_source(dag)
-    sink = get_sink(dag)
-    # print('Source: {}, sink: {}'.format(source, sink))
-    to_visit = set(dag.nodes()).difference(source, sink)
-    # print('to visit: {}'.format(to_visit))
-
-    # The algorithm proceeds by removing any vertex v from this list and
-    # performing as many parallel reductions on the edges incident to
-    # (from) it as it is possible.
-    while to_visit:
-        node = to_visit.pop()
-        parallel_reduce(dag, node, tree) # modifies dag!
-
-        # Now we have either 1. the vertex has a single entering edge and a
-        # single exiting edge, or 2. the vertex still has at least two
-        # distinct in-neighbors or two distinct out-neighbors.  In the
-        # first alternative, the vertex is removed by a series reduction
-        # and the two vertices adjacent to it added to the unsatisfied list
-        # if they are not there already.
-        if dag.in_degree()[node] == 1 and dag.out_degree()[node] == 1:
-            parent = get_sole_parent(dag, node)
-            child = get_sole_child(dag, node)
-            if parent not in [source, sink]:
-                to_visit.add(parent)
-            if child not in [source, sink]:
-                to_visit.add(child)
-
-            series_reduce(dag, node, tree, parent, child) # modifies dag!
-
-        else:
-            # We've found an invalid node.  This is not a TTSP.
-            print('{} is invalid.'.format(node))
+        if not children:
             return
 
-    # This process is repeated until the unsatisfied list becomes empty, at
-    # which point the same process is applied to the source and the sink
-    # (in order to eliminate any multiple edges between them) before
-    # stopping.
-    parallel_reduce(dag, source, tree) # modifies dag!
+        if parent in children:
+            children.remove(parent)
 
-    # The following line is not necessary as at this point there are no
-    # more nodes other than source and sink, and all edges must be parallel
-    # between them.
-    # parallel_reduce(dag, sink)
+        for child in children:
+            self._merge_pnodes_from(child, node)
 
-    # The unsatisfied list becomes empty, either because all vertices
-    # (except source and sink) have been deleted by series reductions or
-    # because every remaining vertex has two distinct in-neighbors or two
-    # distinct out-neighbors. In the first case the DAG is TTSP; in the
-    # second it is not.
-    print('Remaining edges in  dag: {}'.format(dag.edges()))
-    return tree
+        if self.is_pnode(node):
+            for child_pnode in (c for c in children if self.is_pnode(c)):
+                grandchildren = self.neighbors(child_pnode)
+                grandchildren.remove(node)
+
+                self.remove_edges_from((child_pnode, gc) for gc in grandchildren)
+                self.add_edges_from((node, gc) for gc in grandchildren)
+
+                self.remove_edge(node, child_pnode)
+                self.remove_node(child_pnode)
+
 
 
 def main():
     """Read a DAG from stdin, and decompose it if possible."""
-    tree = decompose(read_dag())
+    tree = DecompositionTree()
+    dag = DAG.read_dag()
+    tree.decompose(dag)
+    tree.merge_pnodes()
 
     print('Nodes in tree ({}): '.format(tree.number_of_nodes()))
     for node in tree.nodes():
