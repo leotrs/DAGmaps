eae5e24b2568facf07336186bc0c97f6790fb552
Syed Arefinul Haque <arefindk@gmail.com>
2016-12-06 20:57:09 -0500

    add upload

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express')
 var fs = require('fs')
+var fileUpload = require('express-fileupload');
 var app = express();
 var path = require('path');
 var port = process.env.PORT || 3000;
 var assert = require('assert');
 var PythonShell = require('python-shell');
+
 
 var synthetic = new PythonShell('synthetic_ttsp.py');
 var decomposition = require('child_process').spawn('python3', ['decomposition.py']);
@@ -23,7 +25,43 @@ synthetic.end(function(err) {
 	});
 });
 
+function decompose(){
+	var decomposition = require('child_process').spawn('python3', ['decomposition.py']);
+	fs.readFile('uploads/uploaded_ttsp.txt', function(err, data) {
+		if (err) {
+			return console.log(err);
+		}
+		console.log(data);
+		decomposition.stdin.write(data);
+		// End data write
+		decomposition.stdin.end();
+	});
+}
+
 app.use(express.static('public'))
+
+app.use(fileUpload());
+
+app.post('/upload', function(req, res) {
+    var sampleFile;
+ 
+    if (!req.files) {
+        res.send('No files were uploaded.');
+        return;
+    }
+ 
+    sampleFile = req.files.sampleFile;
+    sampleFile.mv('uploads/uploaded_ttsp.txt', function(err) {
+        if (err) {
+            res.status(500).send(err);
+        }
+        else {
+        	decompose();
+        	return res.redirect('/');
+        }
+    });
+});
+
 
 //routes
 app.get('/', function(req, res) {
